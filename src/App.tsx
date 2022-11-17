a2142b16c1860b4332b8bee866c1db268cbf8530
Aleksander Margiste <masterinbusiness@outlook.com>
2022-11-17 21:37:37 +0100

    Implemented the test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+import useApi from "./hooks/useApi";
+import AddTag from "./components/AddTag";
+import Tag from "./components/Tag";
+import { ITag } from "./interfaces/Tag.interface";
+
 import "./App.css";
 
 function App() {
+  const [supplier, setSupplier] = useState<any>();
+
+  const { getSupplier } = useApi();
+
+  useEffect(() => {
+    setSupplier(getSupplier());
+  }, []);
+
+  const addTag = (name: string) => {
+    const newId = supplier?.['tags-general']?.length > 0
+      ? supplier['tags-general'][supplier['tags-general'].length - 1].id + 1
+      : 1;
+
+    setSupplier((prev: any) => ({
+      ...prev,
+      'tags-general': [
+        ...(prev['tags-general'] || []),
+        {
+          id: newId,
+          name,
+          type: 'supplierBranch-general',
+        },
+      ],
+    }));
+  };
+
   return (
     <div className="border m-12 h-full rounded-xl border-gray-600 p-12 gap-y-4 flex flex-col">
       <h1 className="text-2xl">
@@ -13,13 +46,21 @@ function App() {
       </p>
       <pre className="p-4 rounded-xl bg-gray-200 text-xs">
         {/* Add the JSON Output here */}
+        {supplier ? JSON.stringify(supplier, null, 2) : ''}
       </pre>
       <p className="">
         2. Please display all items under "tags-general" as a Tag. You can use
         the Tag component where the text in the component should be the name
         attribute.
       </p>
-      <div className="flex gap-2">{/* Add Tags here */}</div>
+      <div className="flex gap-2">
+        {/* Add Tags here */}
+        {(supplier?.['tags-general'] || []).map((tag: ITag) => (
+          <Tag key={tag.id}>{tag.name}</Tag>
+        ))}
+
+        <AddTag addTag={addTag} />
+      </div>
       <p className="">
         3. Please add an input on the right of the last Tag and allow the user
         to add a new tag by entering a value and then submitting it using{" "}
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-interface ITag {
+interface ITagProps {
   children: string;
 }
-const Tag: React.FC<ITag> = ({ children }) => {
+const Tag: React.FC<ITagProps> = ({ children }) => {
   return (
     <div className="bg-indigo-300 rounded-md px-2 py-1 text-xs">{children}</div>
   );
